043fac563b18fcbba8e1b3aa40ebe170ee782394
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2024-05-06 16:19:34 +0530

    feat: api route and folder structure

---
+++ components/filter_section/filterSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./filterSection.module.css"
+
+const FilterSection = () => {
+  return (
+    <div className={`${style.wrapper}`}>FilterSection</div>
+  )
+}
+
+export default FilterSection
---
+++ pages/api/fetch_job_listings.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
+  const { limit = 10, offset = 0 }: { limit?: number; offset?: number } = req.body;
+
+  try {
+    const response = await axios.post( "https://api.weekday.technology/adhoc/getSampleJdJSON", {
+      limit,
+      offset
+    } );
+
+    res.status( 200 ).json( { data: response.data } );
+    console.log( response.data );
+  } catch ( error ) {
+    res.status( 500 ).json( { error} );
+    console.error( error );
+  }
+}
